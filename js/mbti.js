007a1cd1a7a6e2982e168d51ef0773c52bd30699
dbtkdok <57377065+dbtkdok@users.noreply.github.com>
2022-02-15 13:32:47 +0900

    Update mbti.js

--- js/mbti.js
+++ js/mbti.js
@@ -2,5 +2,10 @@ $(function () {
 	$(".box").click(function () {
 		$(".box").removeClass('large');
 		$(this).toggleClass('large');
+
+		var id = $(this).attr('id');
+		$('.modal-body *').remove();
+		$('.modal-body').append('<img src="./bingo/' + id + '_bingo.jpg" width="100%">');
+		$('#myModal').modal('toggle');
 	});
 });
